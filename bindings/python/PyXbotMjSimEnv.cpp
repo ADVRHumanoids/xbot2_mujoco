b5526df5b754dd6949e8f08f3200579760b19b32
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-08-12 23:52:22 +0200

    added python unittest for stepping + fixed undef refs issues

--- bindings/python/PyXbotMjSimEnv.cpp
+++ bindings/python/PyXbotMjSimEnv.cpp
@@ -1,4 +1,7 @@
 #include <pybind11/pybind11.h>
+#include "../../src/xmj_sim_env.h"
+
+namespace py = pybind11;
 
 inline bool isRelease() {
     #ifdef IS_RELEASE
@@ -11,6 +14,14 @@ inline bool isRelease() {
 PYBIND11_MODULE(PyXbotMjSimEnv, m) {
 
     m.doc() = "pybind11 for xbot2_mujoco's XbotMjSimEnv class";
+
+    py::class_<XBotMjSimEnv>(m, "XBotMjSimEnv")
+            .def(py::init<const std::string, const char*, bool, bool>(),  py::arg("xbot2_cfg_path"), py::arg("model_fname") = "", py::arg("headless") = false, py::arg("multithread") = true)
+            .def("step", &XBotMjSimEnv::step)
+            .def("render_window", &XBotMjSimEnv::render_window)
+            .def("reset", &XBotMjSimEnv::reset)
+            .def("close", &XBotMjSimEnv::close)
+            ;
 
 }
 
--- tests/PyXBotMjSimEnv/test_stepping.py
+++ tests/PyXBotMjSimEnv/test_stepping.py
@@ -1,17 +1,30 @@
 import unittest
-import numpy as np
 
-import timeit
+from xbot2_mujoco.PyXbotMjSimEnv import XBotMjSimEnv
 
 class TestSimStepping(unittest.TestCase):
 
+    def test_xmj_env(self):
+
+        n_steps=10000
+        n_steps_done=0
+        for i in range(n_steps):
+            self._xmj_env.step()
+            n_steps_done+=1
+            print(f"[test_xmj_env]: step idx: {i}\n");
+
+        self.assertTrue(n_steps_done==n_steps_done)
+
     def setUp(self):
 
-        a=1
+        self._xmj_env = XBotMjSimEnv(xbot2_cfg_path="",
+            model_fname="",
+            headless=True,
+            multithread=True)
 
     def tearDown(self):
 
-        a=1
+        self._xmj_env.close()
 
 if __name__ == "__main__":
 
