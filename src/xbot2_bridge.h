d6a3566d79b336e2c4ab7f4e82ab93db793be514
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-02-21 16:27:53 +0100

    send clock to xbot2

--- src/xbot2_bridge.h
+++ src/xbot2_bridge.h
@@ -5,6 +5,8 @@
 #include "xbot2_mj_joint.h"
 #include "xbot2_mj_linkstate.h"
 
+#include <xbot2/ipc/pipe.h>
+
 namespace XBot {
 
 class MjWrapper
@@ -20,6 +22,16 @@ public:
         _ls(mj_model, cfg_path)
     {
         std::cout << "************ xbot2 bridge constructed with file: " << cfg_path << "\n";
+
+        /* Create mq for clock */
+        using namespace std::chrono_literals;
+        chrono::simulated_clock::enable_sim_time(true);
+        chrono::simulated_clock::initialize(0s);
+
+        _clock_sender = Transmitter::Create("mq",
+                                            "gz_to_xbot2_time",
+                                            10,
+                                            sizeof(std::chrono::nanoseconds));
     }
 
     void run(mjData * d)
@@ -27,12 +39,18 @@ public:
         _joint.run(d);
         _imu.run(d);
         _ls.run(d);
+
+        using ns = std::chrono::nanoseconds;
+        ns clock_msg = std::chrono::duration_cast<ns>(std::chrono::duration<double>(d->time));
+        _clock_sender->try_send(clock_msg);
     }
 private:
 
     JointMjServer _joint;
     ImuMjServer _imu;
     LinkStateMjServer _ls;
+
+    Transmitter::Ptr _clock_sender;
 
 };
 
